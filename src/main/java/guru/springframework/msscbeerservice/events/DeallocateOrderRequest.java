b94b8819a3bf19a97da25cfc373efc923013107e
Saimir Gasa <saimir.gasa@gmail.com>
2020-11-24 00:55:25 +0100

    Added validations.

---
+++ src/main/java/guru/springframework/msscbeerservice/events/DeallocateOrderRequest.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbeerservice.events;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by jt on 2/29/20.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DeallocateOrderRequest {
+    private BeerOrderDto beerOrderDto;
+}
